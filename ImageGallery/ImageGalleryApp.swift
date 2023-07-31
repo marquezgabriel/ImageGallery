24060f1ff3f36bffe775eb67ddbbe3b6f0b0c89e
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-31 10:47:54 -0400

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Gabriel Marquez on 2023-07-31.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
